b9dd879d7c26af81d369530cb0b7957fa24b93c3
philoniare <philoniare@gmail.com>
2016-07-08 11:37:51 -0400

    Initialed starter project for ng2

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -5,12 +5,10 @@ import {
   it
 } from '@angular/core/testing';
 
-// Load the implementations that should be tested
 import { App } from './app.component';
 import { AppState } from './app.service';
 
 describe('App', () => {
-  // provide our implementations or mocks to the dependency injector
   beforeEachProviders(() => [
     AppState,
     App
